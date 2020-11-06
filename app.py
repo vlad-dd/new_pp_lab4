fec676ccf8f1cee5d5ae3829a12c03a44c6eb5c5
Air-Vlad <syrotiukvlad@gmail.com>
2020-11-06 02:22:16 +0200

    all files was add

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/api/v1/hello-world-{27}')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World, 27!'
 
 
 if __name__ == '__main__':
