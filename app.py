4336c7e2b28863ff70b40d61087e990da0565081
Air-Vlad <syrotiukvlad@gmail.com>
2020-11-06 02:47:28 +0200

    +get method

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/api/v1/hello-world-{27}')
+@app.route('/api/v1/hello-world-{27}', methods=['GET'])
 def hello_world():
     return 'Hello World, 27!'
 
